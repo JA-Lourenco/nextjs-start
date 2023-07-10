76c77e7bdf15902ef073a945485ec48aa337fcfa
JA-Lourenco <ja.lourenco@outlook.com>
2023-07-09 22:03:23 -0300

    Creating dashboard: second part

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,6 +4,8 @@ import { Textarea } from "@/components/Textarea";
 
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/react";
+import { FiShare2 } from "react-icons/fi";
+import { FaTrash } from "react-icons/fa";
 
 import styles from "./styles.module.css";
 
@@ -36,6 +38,28 @@ export default function Dashboard() {
               </button>
             </form>
           </div>
+        </section>
+
+        <section className={styles.taskContainer}>
+          <h1>My Tasks</h1>
+
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PUBLIC</label>
+
+              <button type="button" className={styles.shareButton}>
+                <FiShare2 size={22} color="var(--blue)" />
+              </button>
+            </div>
+
+            <div className={styles.taskContent}>
+              <p>my fiurst task example</p>
+
+              <button type="button" className={styles.trashButton}>
+                <FaTrash size={24} color="var(--red)" />
+              </button>
+            </div>
+          </article>
         </section>
       </main>
     </div>
