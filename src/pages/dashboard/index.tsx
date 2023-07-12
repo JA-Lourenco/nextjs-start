e1ca027849712e2edefd576d4eca4a7691621c1c
JA-Lourenco <ja.lourenco@outlook.com>
2023-07-11 22:38:32 -0300

    Listing tasks

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { useState, ChangeEvent, FormEvent } from "react";
+import { useState, useEffect, ChangeEvent, FormEvent } from "react";
 
 import Head from "next/head";
 
@@ -10,7 +10,14 @@ import { FiShare2 } from "react-icons/fi";
 import { FaTrash } from "react-icons/fa";
 
 import { db } from "@/services/firebase";
-import { addDoc, collection } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  query,
+  orderBy,
+  where,
+  onSnapshot,
+} from "firebase/firestore";
 
 import styles from "./styles.module.css";
 
@@ -20,7 +27,16 @@ interface DashboardProps {
   };
 }
 
+interface TaskProps {
+  id: string;
+  created: Date;
+  public: boolean;
+  task: string;
+  user: string;
+}
+
 export default function Dashboard({ user }: DashboardProps) {
+  const [tasks, setTasks] = useState<TaskProps[]>([]);
   const [input, setInput] = useState("");
   const [publicTask, setPublicTask] = useState(false);
 
@@ -50,6 +66,58 @@ export default function Dashboard({ user }: DashboardProps) {
       console.error("handleRegisterTask Error: ", e);
     }
   }
+
+  async function loadTasks() {
+    const tasks = collection(db, "task");
+    const qry = query(
+      tasks,
+      orderBy("created", "desc"),
+      where("user", "==", user?.email)
+    );
+
+    onSnapshot(qry, (snapshot) => {
+      let tasksData = [] as TaskProps[];
+      snapshot.forEach((doc) => {
+        tasksData.push({
+          id: doc.id,
+          task: doc.data().task,
+          created: doc.data().created,
+          user: doc.data().user,
+          public: doc.data().public,
+        });
+      });
+
+      setTasks(tasksData);
+    });
+  }
+
+  function showTasks() {
+    return tasks.map((task) => (
+      <article key={task.id} className={styles.task}>
+        {task.public && (
+          <div className={styles.tagContainer}>
+            <label className={styles.tag}>PUBLIC</label>
+
+            <button type="button" className={styles.shareButton}>
+              <FiShare2 size={22} color="var(--blue)" />
+            </button>
+          </div>
+        )}
+
+        <div className={styles.taskContent}>
+          <p>{task.task}</p>
+
+          <button type="button" className={styles.trashButton}>
+            <FaTrash size={24} color="var(--red)" />
+          </button>
+        </div>
+      </article>
+    ));
+  }
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -92,23 +160,7 @@ export default function Dashboard({ user }: DashboardProps) {
         <section className={styles.taskContainer}>
           <h1>My Tasks</h1>
 
-          <article className={styles.task}>
-            <div className={styles.tagContainer}>
-              <label className={styles.tag}>PUBLIC</label>
-
-              <button type="button" className={styles.shareButton}>
-                <FiShare2 size={22} color="var(--blue)" />
-              </button>
-            </div>
-
-            <div className={styles.taskContent}>
-              <p>my fiurst task example</p>
-
-              <button type="button" className={styles.trashButton}>
-                <FaTrash size={24} color="var(--red)" />
-              </button>
-            </div>
-          </article>
+          {showTasks()}
         </section>
       </main>
     </div>
